d46241fd01d9f6119b9411d224838773f0eb2008
ivansteb <steblerivan@gmail.com>
2025-08-11 21:54:29 -0300

    add Firebase emulator configuration for development environment

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,28 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import {
+  connectAuthEmulator,
+  getAuth,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+
+// Determinar qué configuración usar según el entorno
+const isDevelopment = process.env.NODE_ENV === "development";
 
 // Es seguro exponer estas claves en el frontend.
 // Firebase lo gestiona con sus reglas de seguridad.
-const firebaseConfig = {
+
+const devConfig = {
+  apiKey: "AIzaSyDW_NGA417cyDaZIHgEq0C5R497TTd-IGA",
+  authDomain: "dev-movie-and-beer-app.firebaseapp.com",
+  projectId: "dev-movie-and-beer-app",
+  storageBucket: "dev-movie-and-beer-app.firebasestorage.app",
+  messagingSenderId: "221161028723",
+  appId: "1:221161028723:web:a8e155c94fc9254e367e06",
+  measurementId: "G-41B608CJ36",
+};
+
+const prodConfig = {
   apiKey: "AIzaSyCLf5GIjsUWf9dC5do2EeYjkZodCYxVtgo",
   authDomain: "movie-and-beer-app.firebaseapp.com",
   projectId: "movie-and-beer-app",
@@ -14,6 +32,8 @@ const firebaseConfig = {
   measurementId: "G-HCDSNDGXFJ",
 };
 
+const firebaseConfig = isDevelopment ? devConfig : prodConfig;
+
 // Inicializar la app de Firebase
 const app = initializeApp(firebaseConfig);
 
@@ -21,3 +41,11 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+
+// Usar emuladores en desarrollo local si la variable está presente
+const useEmulators = import.meta.env.VITE_USE_FIREBASE_EMULATORS === "true";
+if (useEmulators) {
+  connectFirestoreEmulator(db, "localhost", 8080);
+  connectAuthEmulator(auth, "http://localhost:9099", { disableWarnings: true });
+  console.log("--- Usando emuladores de Firebase!");
+}
